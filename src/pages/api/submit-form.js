741aaa74d700cc32db50953a3b6b1575c4349e6d
premkumar <premkumarananthan1@gmail.com>
2025-05-09 16:10:35 +0530

    url updated

--- src/pages/api/submit-form.js
+++ src/pages/api/submit-form.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', 'https://agriculture-iota-murex.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
